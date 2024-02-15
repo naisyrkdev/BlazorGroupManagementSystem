bcfb99f8a149ebe03bed827907afa7fa8d9014de
krystian grzelak <krystian.grzelak@protonmail.com>
2024-02-15 15:48:28 +0100

    Clearing up template solution part1, added MudBlazor for Drag&Drop API

--- BlazorGroupManagementSystem/Program.cs
+++ BlazorGroupManagementSystem/Program.cs
@@ -1,21 +1,14 @@
-using BlazorGroupManagementSystem.Data;
-using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Web;
+using BlazorGroupManagementSystem;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddRazorPages();
-builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<WeatherForecastService>();
+ServicesConfiguration.AddApplicationServices(builder.Services);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
---
+++ BlazorGroupManagementSystem/ServicesConfiguration.cs
@@ -0,0 +1,15 @@
+﻿
+namespace BlazorGroupManagementSystem;
+using MudBlazor.Services;
+
+public static class ServicesConfiguration
+{
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+    {
+        services.AddRazorPages();
+        services.AddServerSideBlazor();
+        services.AddMudServices();
+
+        return services;
+    }
+}
