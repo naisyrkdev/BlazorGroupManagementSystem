0ad1db5480d4139335ec628ad42f92f8a470862e
krystian grzelak <krystian.grzelak@protonmail.com>
2024-02-15 16:05:58 +0100

    Clearing up, added client models, nav menu changes, setting up MudBlazor

---
+++ BlazorGroupManagementSystem/Models/GroupChangeLogClientModel.cs
@@ -0,0 +1,7 @@
+﻿namespace BlazorGroupManagementSystem.Models;
+
+public class GroupChangeLogClientModel
+{
+    public string Value { get; set; } = String.Empty;
+    public DateTime Date { get; set; }
+}
---
+++ BlazorGroupManagementSystem/Models/UserClientModel.cs
@@ -0,0 +1,20 @@
+﻿namespace BlazorGroupManagementSystem.Models;
+
+public class UserClientModel
+{
+    public string FirstName { get; init; } = String.Empty;
+    public string LastName { get; init; } = String.Empty;
+    public string ImgPath { get; init; } = String.Empty;
+    public GroupEnum Group { get; set; }
+    public string City { get; set; } = String.Empty;
+}
+
+public enum GroupEnum
+{
+    NoGroup,
+    Group1,
+    Group2,
+    Group3,
+    Group4,
+    Group5
+}
